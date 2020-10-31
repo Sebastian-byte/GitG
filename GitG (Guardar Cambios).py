9e125c0bb77b315eb60268cb8ebcd2bc605a4b56
sebastian-byte <sebatianmontoya209@gmail.com>
2020-10-31 18:29:23 -0500

    Login Manager

--- GitG (Guardar Cambios).py
+++ GitG (Guardar Cambios).py
@@ -1,7 +1,9 @@
 import os
 import platform
+from pathlib import Path
 
 system = platform.system()
+home = str(Path.home())
 
 if system == 'Windows':
     pause = 'pause'
@@ -13,23 +15,52 @@ else:
     print('Sistema no compatible!')
     quit()
 
-print('Git Graphical (GitG) (Guardar Cambios)')
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-print('\nTip: Si ya estas en el directorio pon "."')
+def Main():
+    print('Git Graphical (GitG) (Guardar Cambios)')
 
-# Pedirle el directorio al usuario
-dirName = input(('Directorio del repositorio: '))
+    print('\nTip: Si ya estas en el directorio pon "."')
 
-# Nombre del commit
-commitName = input(('\nNombre del commit: '))
+    # Pedirle el directorio al usuario
+    dirName = input(('Directorio del repositorio: '))
 
-os.chdir(dirName)
+    # Nombre del commit
+    commitName = input(('\nNombre del commit: '))
 
-os.system('git add .')
+    os.chdir(dirName)
 
-os.system('git commit -m "' + commitName + '"')
+    os.system('git add .')
 
-os.system('git push -u -f origin main')
+    os.system('git commit -m "' + commitName + '"')
 
-print('\nTodo Listo!')
-os.system(pause)
+    os.system('git push -u -f origin main')
+
+    print('\nTodo Listo!')
+    os.system(pause)
+
+# Revisar si el usuario ya inicio sesión.
+if os.path.exists(home + '/.gitconfig'):
+    Main()
+# Si no lo ha hecho, Pedirle que lo haga.
+else:
+    print('Git Graphical (GitG) (Guardar Cambios)')
+    print('\nTienes que iniciar sesion en Github!')
+    # Pedir nombre al usuario
+    username = input(('\nNombre de usuario: '))
+    # Pedir correo al usuario
+    email = input(('Correo: '))
+    # Pedir contraseña al usuario
+    password = input(('Contraseña: '))
+    # Guardar informacion en archivo .gitconfig
+    os.system('git config --global user.name "' + username + '"')
+    os.system('git config --global user.email ' + email)
+    os.system('git config --global user.password "' + password + '"')
+    print('\nInicio exitoso!')
+    os.system(pause)
+    clear() # Limpiar la consola
+    Main() # Llamar a la funcion principal
--- GitG.py
+++ GitG.py
@@ -1,7 +1,9 @@
-import os
+﻿import os
 import platform
+from pathlib import Path 
 
 system = platform.system()
+home = str(Path.home())
 
 if system == 'Windows':
     pause = 'pause'
@@ -13,35 +15,66 @@ else:
     print('Sistema no compatible!')
     quit()
 
-print('Git Graphical (GitG)')
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 
-print('\nTip: Si ya estas en el directorio pon "."')
+# Funcion principal
+def Main():
+    print('Git Graphical (GitG)')
+    
+    print('\nTip: Si ya estas en el directorio pon "."')
 
-# Pedirle el directorio al usuario
-dirName = input(('Directorio del repositorio: '))
+    # Pedirle el directorio al usuario
+    dirName = input(('Directorio del repositorio: '))
 
-# Nombre del commit inicial
-commitName = input(('\nNombre del commit: '))
+    # Nombre del commit inicial
+    commitName = input(('\nNombre del commit: '))
 
-# Nombre del usuario, para agregar a la URL
-user = input(('\nNombre de usuario: '))
+    # Nombre del usuario, para agregar a la URL
+    user = input(('\nNombre de usuario: '))
 
-# Nombre del repositorio a hacer commit
-repo = input(('\nNombre del repositorio: '))
+    # Nombre del repositorio a hacer commit
+    repo = input(('\nNombre del repositorio: '))
 
-os.chdir(dirName)
+    os.chdir(dirName)
 
-os.system('git init')
+    os.system('git init')
 
-os.system('git add .')
+    os.system('git add .')
 
-os.system('git commit -m "' + commitName + '"')
+    os.system('git commit -m "' + commitName + '"')
 
-os.system('git branch -M main')
+    os.system('git branch -M main')
 
-os.system('git remote add origin https://github.com/' + user + '/' + repo + '/')
+    os.system('git remote add origin https://github.com/' + user + '/' + repo + '/')
 
-os.system('git push -u -f origin main')
+    os.system('git push -u -f origin main')
 
-print('\nTodo Listo!')
-os.system(pause)
+    print('\nTodo Listo!')
+    os.system(pause)
+
+# Revisar si el usuario ya inicio sesión.
+if os.path.exists(home + '/.gitconfig'):
+    Main()
+# Si no lo ha hecho, Pedirle que lo haga.
+else:
+    print('Git Graphical (GitG)')
+    print('\nTienes que iniciar sesion en Github!')
+    # Pedir nombre al usuario
+    username = input(('\nNombre de usuario: '))
+    # Pedir correo al usuario
+    email = input(('Correo: '))
+    # Pedir contraseña al usuario
+    password = input(('Contraseña: '))
+    # Guardar informacion en archivo .gitconfig
+    os.system('git config --global user.name "' + username + '"')
+    os.system('git config --global user.email ' + email)
+    os.system('git config --global user.password "' + password + '"')
+    print('\nInicio exitoso!')
+    os.system(pause)
+    clear() # Limpiar la consola
+    Main() # Llamar a la funcion principal
+    
