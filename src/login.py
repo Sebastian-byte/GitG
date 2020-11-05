49fb2fa1ab42fa8c673f108f895ae3b380c6ac43
Sebastian-byte <sebastianmontoya209@gmail.com>
2020-11-05 18:45:51 -0500

    Remove Trailing Space

--- src/login.py
+++ src/login.py
@@ -8,49 +8,49 @@ def requestLogin(come):
 
     if come == 1:
         print('Git Graphical (GitG)')
-        
+
         print('\nTienes que iniciar sesion en Github!')
-        
+
         # Pedir nombre al usuario
         username = input(('\nNombre de usuario: '))
-        
+
         # Pedir correo al usuario
         email = input(('Correo: '))
-        
+
         # Pedir contraseña al usuario
         password = input(('Contraseña: '))
-        
+
         # Guardar informacion en archivo .gitconfig
         os.system('git config --global user.email ' + email)
         os.system('git config --global user.name "' + username + '"')
         os.system('git config --global user.password "' + password + '"')
-        
+
         print('\nInicio exitoso!')
-        
+
         pause() # Pausar la consola
         clear() # Limpiar la consola
         Main() # Llamar a la funcion principal
     elif come == 2:
         print('Git Graphical (GitG)')
-        
+
         print('\nTienes que iniciar sesion en Github!')
-        
+
         # Pedir nombre al usuario
         username = input(('\nNombre de usuario: '))
-        
+
         # Pedir correo al usuario
         email = input(('Correo: '))
-        
+
         # Pedir contraseña al usuario
         password = input(('Contraseña: '))
-        
+
         # Guardar informacion en archivo .gitconfig
         os.system('git config --global user.email ' + email)
         os.system('git config --global user.name "' + username + '"')
         os.system('git config --global user.password "' + password + '"')
-        
+
         print('\nInicio exitoso!')
-        
+
         pause() # Pausar la consola
         clear() # Limpiar la consola
         MainSave() # Llamar a la funcion principal
